c271dc2dd31f7e5cb58e12675953a9102e3f593d
DESKTOP-422GIVF\ADMIN <quangdt@falcongames.com>
2025-08-06 09:24:51 +0700

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,129 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    [SerializeField]
+    private float moveForce = 10f;
+
+    [SerializeField]
+    private float jumpForce = 8f;
+
+    private float movementX;
+
+    private Rigidbody2D myBody;
+
+    private SpriteRenderer sr;
+
+    private Animator anim;
+
+    private string WALK_ANIMATION = "Walk";
+
+    private bool isGrounded;
+
+    private bool jumpRequested = false;
+
+    private string GROUND_TAG = "Ground";
+
+    private void Awake()
+    {
+
+        myBody = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+        sr = GetComponent<SpriteRenderer>();
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(myBody.bodyType);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayerMoveKeyboard();
+        AnimatePlayer();
+
+        if ((Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow)) && isGrounded)
+        {
+            jumpRequested = true;
+        }
+
+        //Debug.Log("isGrounded: " + isGrounded);
+    }
+
+    private void FixedUpdate()
+    {
+        if (jumpRequested)
+        {
+            PlayerJump();
+            jumpRequested = false;
+        }
+        //Debug.Log("FixedUpdate called");
+
+    }
+
+    void PlayerMoveKeyboard()
+    {
+
+        movementX = Input.GetAxisRaw("Horizontal");
+
+        transform.position += new Vector3(movementX, 0f, 0f) * moveForce * Time.deltaTime;
+
+    }
+
+    void AnimatePlayer()
+    {
+
+        if (movementX > 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = false;
+        }
+        else if (movementX < 0)
+        {
+            anim.SetBool(WALK_ANIMATION, true);
+            sr.flipX = true;
+        }
+        else
+        {
+            anim.SetBool(WALK_ANIMATION, false);
+        }
+
+    }
+
+    void PlayerJump()
+    {
+        jumpRequested = false;
+        isGrounded = false;
+        myBody.AddForce(new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        Debug.Log("Jumped");
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(GROUND_TAG))
+            isGrounded = true;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(GROUND_TAG))
+            isGrounded = true;
+        Debug.Log(collision.gameObject.name + " isGrounded: " + isGrounded);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag(GROUND_TAG))
+            isGrounded = false;
+    }
+}
+
+
